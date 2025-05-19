1526757908c6c913c2226a1b672debb5a4bac4e9
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-05-19 18:24:09 +0200

    Fix: added server health

--- server/server.ts
+++ server/server.ts
@@ -3,40 +3,38 @@ import getUserInfo from './request/userProfile/getUserInfo'
 
 const app = Fastify();
 
-// Définir le port
 const port = 3001;
 
-// Définir un type pour les paramètres de la route
 interface ProfileParams {
   username: string;
 }
 
-// Route d'accueil
 app.get('/', async (req, res) => {
 	return 'Hello World!';
 });
 
-// Route pour obtenir le profil utilisateur
 app.get('/profile/:username', async function (request, reply) {
 	const { username } = request.params as { username: string }
 
-	// Vérifier que le paramètre username est fourni
-	if (!username) {
+	if (!username)
+	{
 		console.log('No username passed as parameter in profile route')
 		return reply.code(400).send({ error: 'Username is required' })
 	}
 
 	try {
-		// Appeler la fonction getUserInfo pour récupérer les infos de l'utilisateur
 		const result = await getUserInfo(username)
-		return reply.send(result)  // Renvoie la réponse avec les données de l'utilisateur
+		return (reply.send(result))
 	} catch (err) {
-		console.log('No user found in profileRoute', err)
-		reply.code(404).send({ error: 'User not found' })  // Si l'utilisateur n'est pas trouvé, renvoie une erreur 404
+		console.log('No user found in profileRoute')
+		reply.code(404).send({ error: 'User not found' })
 	}
 })
 
-// Démarrer le serveur avec un objet de configuration
+app.get('/health', async (req, reply) => {
+	return reply.code(200).send({ status: 'ok' });
+});
+
 app.listen({ port, host: '0.0.0.0' }, (err, address) => {
 	if (err) {
 		console.log(err);
