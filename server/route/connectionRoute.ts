75534a59cbb80b4bf1bf3be794f34ab94c2df6bf
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-05-19 17:06:12 +0200

    Fix: split route

--- server/route/connectionRoute.ts
+++ server/route/connectionRoute.ts
@@ -7,40 +7,39 @@ import { connectionData } from '../utils/interface'
 const server = fastify()
 
 server.get('/register', async (request, reply) => {
-    const { data } = request.query as { data: connectionData }
-  
-    try {
-      connectionSchema.parse(data)
-    } catch (err) {
-      console.log('Data not conform in registerRoute')
-      return reply.code(400).send({ error: 'Invalid data' })
-    }
-  
-    try {
-      await register(data)
-      return reply.code(201).send({ message: 'User registered successfully' })
-    } catch (err) {
-      console.log('Could not register the user')
-      return reply.code(500).send({ error: 'Could not register the user' })
-    }
+		const { data } = request.query as { data: connectionData }
+
+		try {
+			connectionSchema.parse(data)
+		} catch (err) {
+			console.log('Data not conform in registerRoute')
+			return reply.code(400).send({ error: 'Invalid data' })
+		}
+
+		try {
+			await register(data)
+			return reply.code(201).send({ message: 'User registered successfully' })
+		} catch (err) {
+			console.log('Could not register the user')
+			return reply.code(500).send({ error: 'Could not register the user' })
+		}
 })
 
 server.get('/login', async (request, reply) => {
-    const { data } = request.query as { data: connectionData }
-  
-    try {
-      connectionSchema.parse(data)
-    } catch (err) {
-      console.log('Data not conform in loginRoute')
-      return reply.code(400).send({ error: 'Invalid data' })
-    }
-  
-    try {
-      await login(data)
-      return reply.code(202).send({ message: 'User logged successfully' })
-    } catch (err) {
-      console.log('Could not register the user')
-      return reply.code(500).send({ error: 'Could not log the user' })
-    }
+		const { data } = request.query as { data: connectionData }
+
+		try {
+			connectionSchema.parse(data)
+		} catch (err) {
+			console.log('Data not conform in loginRoute')
+			return reply.code(400).send({ error: 'Invalid data' })
+		}
+
+		try {
+			await login(data)
+			return reply.code(202).send({ message: 'User logged successfully' })
+		} catch (err) {
+			console.log('Could not register the user')
+			return reply.code(500).send({ error: 'Could not log the user' })
+		}
 })
-  
