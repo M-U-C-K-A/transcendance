c306183661f756f0df747f9be569dd4190368ba9
hugo <hugodelacour.pro@gmail.com>
2025-05-19 23:15:28 +0200

    new prisma ORM SCHEMA 12/N

--- server/request/chat/getMessage.ts
+++ server/request/chat/getMessage.ts
@@ -3,16 +3,28 @@ import { PrismaClient } from '@prisma/client'
 const Prisma = new PrismaClient()
 
 export default async function getMessage(username: string) {
-	const message = await Prisma.$queryRaw`SELECT m.*,
-	sender.username as sender_username,
-	recipient.username as recipient_username
-	FROM Message m
-	JOIN User sender ON m.senderId = sender.id
-	JOIN User recipient ON m.recipientId = recipient.id
-	WHERE m.status = true
-	OR sender.username = :votre_username
-	OR recipient.username = :votre_username
-	ORDER BY m.sendAt DESC;`
-
-	return (message)
+  try {
+    const messages = await Prisma.$queryRaw`
+      SELECT
+        m.*,
+        sender.username as sender_username,
+        recipient.username as recipient_username
+      FROM
+        Message m
+      JOIN
+        User sender ON m.senderId = sender.id
+      LEFT JOIN
+        User recipient ON m.recipientId = recipient.id
+      WHERE
+        m.isGeneral = true
+        OR sender.username = ${username}
+        OR recipient.username = ${username}
+      ORDER BY
+        m.sendAt DESC
+    `
+    return messages
+  } catch (error) {
+    console.error("Error fetching messages:", error)
+    throw error
+  }
 }
