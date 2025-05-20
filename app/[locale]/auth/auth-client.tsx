0457b0d958bc556ea8100508bdad8b8af08459b7
Hugo Delacour <admin@mac-m4.local>
2025-05-20 02:57:43 +0200

    fix: Link auth page

--- app/[locale]/auth/auth-client.tsx
+++ app/[locale]/auth/auth-client.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import PongGame from "@/components/landing/PongGame";
+import Link from "next/link";
 
 export function LoginPage() {
   const [isLogin, setIsLogin] = useState(true)
@@ -45,12 +46,9 @@ export function LoginPage() {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Transcendance
-          </a>
+          <Link href="/" className="flex items-center gap-2 font-medium">
+            ft_transcendence
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
