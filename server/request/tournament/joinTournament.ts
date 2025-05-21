1fe0137faa2bef14273d627fcb22d8b13acbb655
Thomasrbm <frozyxyt.76@gmail.com>
2025-05-21 23:01:48 +0200

    Fix: tournament participant

--- server/request/tournament/joinTournament.ts
+++ server/request/tournament/joinTournament.ts
@@ -3,27 +3,28 @@ import { userData }     from '@/server/utils/interface'
 
 const prisma = new PrismaClient()
 
-export async function joinTournament(
-  username: string,
-  tournamentId: number
-): Promise<void> {
+export async function joinTournament(username: string, tournamentId: number): 
+Promise<void> {
+
   const [u] = await prisma.$queryRaw<userData[]>`
     SELECT id
     FROM "User"
     WHERE username = ${username}
-    LIMIT 1
   `
   if (!u) throw new Error('USER NOT FOUND')
 
+
+
   const [info] = await prisma.$queryRaw<{ slot: number; count: number }[]>`
-    SELECT t.slot,
-      (SELECT COUNT(*) FROM "TournamentHistory" WHERE "tournamentId" = ${tournamentId}) AS count
-    FROM "Tournament" AS t
-    WHERE t.id = ${tournamentId}
-    LIMIT 1
+    SELECT Tournament.slot,
+      (SELECT COUNT(*) FROM "TournamentParticipants" WHERE "tournamentId" = ${tournamentId}) AS count
+    FROM "Tournament"
+    WHERE Tournament.id = ${tournamentId}
   `
   if (!info)          throw new Error('TOURNAMENT NOT FOUND')
   if (info.count >= info.slot) throw new Error('TOURNAMENT FULL')
+
+
 
   await prisma.$executeRaw`
     INSERT INTO "TournamentHistory" ("userId","tournamentId")
