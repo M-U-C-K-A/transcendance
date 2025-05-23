66be2c1da3fb8ca772770675d683e39c97807939
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-05-23 02:26:16 +0200

    add: jwt token in backend

--- server/request/match/interface.ts
+++ server/request/match/interface.ts
@@ -1,7 +1,14 @@
 export interface matchInfo{
-	matchName: string
-	p1Name: string
-	p2Name?: string
+	name: string
+	p1Id: string
+	p2Id?: string
 	p1Elo: number
 	p2Elo?: number
+	winnerId: number
+	p1Score: number
+	p2Score: number
+	p1EloGain: number
+	p2EloGain: number
+	mDate: number
+	matchType: string
 }
