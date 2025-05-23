d760d11dfd8fb450aba3e7146fe8694fb055c49b
Thomasrbm <frozyxyt.76@gmail.com>
2025-05-23 03:25:52 +0200

    Fix: for pnpm build

--- app/[locale]/auth/auth-client.tsx
+++ app/[locale]/auth/auth-client.tsx
@@ -5,42 +5,11 @@ import { Register } from "@/components/auth/Register";
 import { useState } from "react";
 import Link from "next/link";
 import { ThemeToggle } from "@/components/theme-toggle";
-import { useTheme } from 'next-themes';
 import Image from "next/image";
 
 export function LoginPage() {
-	const { theme } = useTheme();
 
 	const [isLogin, setIsLogin] = useState(true);
-
-	const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		const formData = new FormData(e.currentTarget);
-		const email = formData.get("email") as string;
-		const username = formData.get("username") as string;
-		const password = formData.get("password") as string;
-
-		try {
-			const response = await fetch("http://localhost:3001/auth/register", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ email, username, password }),
-			});
-
-			const data = await response.json();
-
-			if (!response.ok) {
-				throw new Error(data.message || "Erreur lors de l'inscription");
-			}
-
-			// Rediriger vers la page de connexion après inscription réussie
-			setIsLogin(true);
-		} catch (err) {
-			console.error("Erreur lors de l'inscription:", err);
-		}
-	};
 
 	return (
 		<div className="grid min-h-svh lg:grid-cols-2">
@@ -85,11 +54,10 @@ export function LoginPage() {
 				<Image
 					width={661}
 					height={882}
-					src="/throbertchauve.webp"
+					src=""
 					alt="DuckFace"
 					className="absolute inset-0 w-full h-full object-cover"
 				/>
-				{/* <PongGame /> */}
 			</div>
 		</div>
 	);
