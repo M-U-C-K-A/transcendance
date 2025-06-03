b12e5f47f68aabe6fc16c086df9dc557304764d3
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-06-04 01:35:47 +0200

    fix: remove friend server side

--- server/request/friends/removeFriend.ts
+++ server/request/friends/removeFriend.ts
@@ -2,22 +2,10 @@ import { PrismaClient } from "@prisma/client";
 
 const Prisma = new PrismaClient();
 
-export default async function removeFriend(userId: number, friendName: string) {
-	const friend = await Prisma.user.findUnique({
-		where: {
-			username: friendName,
-		},
-		select: {
-			id: true,
-		}
-	});
-
-	if (!friend) {
-		console.log("Could not find friend Id")
-		throw new Error("Could not find friend Id")
-	}
-
-	const friendId = friend.id
+export default async function removeFriend(userId: number, friendId: number) {
+	console.log('test')
+	console.log(userId)
+	console.log(friendId)
 
 	await Prisma.friends.deleteMany({
 		where: {
