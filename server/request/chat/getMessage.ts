105d5bb69d6fad77471825d131794534791177b2
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-06-04 02:44:02 +0200

    Fix: send message server side

--- server/request/chat/getMessage.ts
+++ server/request/chat/getMessage.ts
@@ -6,46 +6,46 @@ const Prisma = new PrismaClient()
 export default async function getMessage(username: string) {
 	const messages = await Prisma.$queryRaw<any[]>`
 	WITH general_messages AS (
-		SELECT m.*,
-		sender.id as sender_id,
-		sender.username AS sender_username,
-		sender.alias AS sender_alias,
-		sender.win AS sender_win,
-		sender.lose AS sender_lose,
-		sender.elo AS sender_elo,
-		recipient.id as recipient_id,
-		recipient.username AS recipient_username,
-		recipient.alias AS recipient_alias,
-		recipient.win AS recipient_win,
-		recipient.lose AS recipient_lose,
-		recipient.elo AS recipient_elo
-		FROM "Message" m
-		JOIN "User" sender ON sender.id = m."senderId"
-		LEFT JOIN "User" recipient ON recipient.id = m."recipientId"
-		WHERE m."isGeneral" = TRUE
-		ORDER BY m."id" DESC
-		LIMIT 100
+	SELECT m.*,
+	sender.id as sender_id,
+	sender.username AS sender_username,
+	sender.alias AS sender_alias,
+	sender.win AS sender_win,
+	sender.lose AS sender_lose,
+	sender.elo AS sender_elo,
+	recipient.id as recipient_id,
+	recipient.username AS recipient_username,
+	recipient.alias AS recipient_alias,
+	recipient.win AS recipient_win,
+	recipient.lose AS recipient_lose,
+	recipient.elo AS recipient_elo
+	FROM "Message" m
+	JOIN "User" sender ON sender.id = m."senderId"
+	LEFT JOIN "User" recipient ON recipient.id = m."recipientId"
+	WHERE m."isGeneral" = TRUE
+	ORDER BY m."id" DESC
+	LIMIT 100
 	),
 	private_messages AS (
-		SELECT m.*,
-		sender.id as sender_id,
-		sender.username AS sender_username,
-		sender.alias AS sender_alias,
-		sender.win AS sender_win,
-		sender.lose AS sender_lose,
-		sender.elo AS sender_elo,
-		recipient.id as recipient_id,
-		recipient.username AS recipient_username,
-		recipient.alias AS recipient_alias,
-		recipient.win AS recipient_win,
-		recipient.lose AS recipient_lose,
-		recipient.elo AS recipient_elo
-		FROM "Message" m
-		JOIN "User" sender ON sender.id = m."senderId"
-		LEFT JOIN "User" recipient ON recipient.id = m."recipientId"
-		JOIN "User" currentUser ON currentUser.username = ${username}
-		WHERE (m."senderId" = currentUser.id OR m."recipientId" = currentUser.id)
-		AND m."isGeneral" = FALSE
+	SELECT m.*,
+	sender.id as sender_id,
+	sender.username AS sender_username,
+	sender.alias AS sender_alias,
+	sender.win AS sender_win,
+	sender.lose AS sender_lose,
+	sender.elo AS sender_elo,
+	recipient.id as recipient_id,
+	recipient.username AS recipient_username,
+	recipient.alias AS recipient_alias,
+	recipient.win AS recipient_win,
+	recipient.lose AS recipient_lose,
+	recipient.elo AS recipient_elo
+	FROM "Message" m
+	JOIN "User" sender ON sender.id = m."senderId"
+	LEFT JOIN "User" recipient ON recipient.id = m."recipientId"
+	JOIN "User" currentUser ON currentUser.username = ${username}
+	WHERE (m."senderId" = currentUser.id OR m."recipientId" = currentUser.id)
+	AND m."isGeneral" = FALSE
 	)
 	SELECT * FROM general_messages
 	UNION ALL
--- server/request/chat/sendMessage.ts
+++ server/request/chat/sendMessage.ts
@@ -1,27 +1,47 @@
-import { sendMessageData, id } from '@/server/request/chat/interface';
+import { sendMessageData } from '@/server/request/chat/interface';
 import { PrismaClient } from '@prisma/client'
+import { returnData } from './interface';
 
 const Prisma = new PrismaClient()
 
-export default async function sendMessage(senderId: number, data: sendMessageData) {
-
+export default async function sendMessage(sender: number, data: sendMessageData) {
 	let isGeneral: boolean = true;
 	let recipientId: number | null = null;
+	let returnData: returnData = {
+		id: null,
+		username: null
+	}
 
-	if (data.recipientId != -1) {
+	if (data.recipient) {
+		const userInfo = await Prisma.user.findUnique ({
+			where: {
+				username: data.recipient,
+			},
+			select: {
+				id: true,
+				username: true,
+			}
+		})
+
+		if (!userInfo) {
+			console.log ("User not found , Could not send message")
+			throw new Error ("User not found , Could not send message")
+		}
+
+		returnData.id = userInfo.id
+		returnData.username = userInfo.username
 		isGeneral = false
-		recipientId = data.recipientId
 	}
 
 	await Prisma.message.create({
 		data: {
-			senderId,
-			recipientId: recipientId,
+			senderId: sender,
+			recipientId: returnData.id,
 			content: data.content,
 			isGeneral: isGeneral,
 			messageType: data.messageType,
 		},
 	});
 
-	return (true)
+	return (returnData)
 }
