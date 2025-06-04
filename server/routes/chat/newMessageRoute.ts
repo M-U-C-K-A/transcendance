faaa6af06291883cad872ed787b85fe2ee1cdfd7
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-06-04 03:27:27 +0200

    Fix: check if user exist

--- server/routes/chat/newMessageRoute.ts
+++ server/routes/chat/newMessageRoute.ts
@@ -3,12 +3,12 @@ import authMiddleware from "@/server/authMiddleware";
 import newMessage from "@/server/request/chat/newMessage";
 
 export default async function newMessageRoute(server: FastifyInstance) {
-	server.get('/chat/:username', { preHandler: authMiddleware }, async function (request, reply) {
-		const { username } = request.params as { username: string };
+	server.post('/chat/create', { preHandler: authMiddleware }, async function (request, reply) {
+	const { username } = request.body as { username: string };
 
-		if (!username) {
-			return reply.code(400).send({ error: 'username parameter is required' });
-		}
+	if (!username) {
+		return reply.code(400).send({ error: 'username parameter is required' });
+	}
 
 		try {
 			const result = await newMessage(username);
