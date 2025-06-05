21e476cdb0f1d3d22361e3c471aa5c10bcd1443b
ThomasRbm <frozyxyt.76@gmail.com>
2025-06-05 20:04:01 +0200

    Fix: viewport export in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,11 +63,16 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
-  viewport: "width=device-width, initial-scale=1",
   verification: {
     google: "votre-code-verification-google",
   },
 };
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
 
 export default function RootLayout({
   children,
