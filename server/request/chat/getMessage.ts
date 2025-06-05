e6afc677d3a57c9de64b4b6b8da8345c1141a1bb
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-06-05 20:25:21 +0200

    Fix: send message server side

--- server/request/chat/getMessage.ts
+++ server/request/chat/getMessage.ts
@@ -6,52 +6,60 @@ const Prisma = new PrismaClient()
 export default async function getMessage(username: string) {
 	const messages = await Prisma.$queryRaw<any[]>`
 	WITH general_messages AS (
-	SELECT m.*,
-	sender.id as sender_id,
-	sender.username AS sender_username,
-	sender.alias AS sender_alias,
-	sender.win AS sender_win,
-	sender.lose AS sender_lose,
-	sender.elo AS sender_elo,
-	recipient.id as recipient_id,
-	recipient.username AS recipient_username,
-	recipient.alias AS recipient_alias,
-	recipient.win AS recipient_win,
-	recipient.lose AS recipient_lose,
-	recipient.elo AS recipient_elo
-	FROM "Message" m
-	JOIN "User" sender ON sender.id = m."senderId"
-	LEFT JOIN "User" recipient ON recipient.id = m."recipientId"
-	WHERE m."isGeneral" = TRUE
-	ORDER BY m."id" DESC
-	LIMIT 100
+		SELECT
+			m.id,
+			m."senderId",
+			m."recipientId",
+			m."content",
+			m."sendAt",
+			m."readStatus",
+			m."isGeneral",
+			m."messageType",
+			sender.username AS sender_username,
+			sender.win AS sender_win,
+			sender.lose AS sender_lose,
+			sender.elo AS sender_elo,
+			recipient.username AS recipient_username,
+			recipient.win AS recipient_win,
+			recipient.lose AS recipient_lose,
+			recipient.elo AS recipient_elo
+		FROM "Message" m
+		JOIN "User" sender ON sender.id = m."senderId"
+		LEFT JOIN "User" recipient ON recipient.id = m."recipientId"
+		WHERE m."isGeneral" = TRUE
+		ORDER BY m."id" DESC
+		LIMIT 100
 	),
 	private_messages AS (
-	SELECT m.*,
-	sender.id as sender_id,
-	sender.username AS sender_username,
-	sender.alias AS sender_alias,
-	sender.win AS sender_win,
-	sender.lose AS sender_lose,
-	sender.elo AS sender_elo,
-	recipient.id as recipient_id,
-	recipient.username AS recipient_username,
-	recipient.alias AS recipient_alias,
-	recipient.win AS recipient_win,
-	recipient.lose AS recipient_lose,
-	recipient.elo AS recipient_elo
-	FROM "Message" m
-	JOIN "User" sender ON sender.id = m."senderId"
-	LEFT JOIN "User" recipient ON recipient.id = m."recipientId"
-	JOIN "User" currentUser ON currentUser.username = ${username}
-	WHERE (m."senderId" = currentUser.id OR m."recipientId" = currentUser.id)
-	AND m."isGeneral" = FALSE
+		SELECT
+			m.id,
+			m."senderId",
+			m."recipientId",
+			m."content",
+			m."sendAt",
+			m."readStatus",
+			m."isGeneral",
+			m."messageType",
+			sender.username AS sender_username,
+			sender.win AS sender_win,
+			sender.lose AS sender_lose,
+			sender.elo AS sender_elo,
+			recipient.username AS recipient_username,
+			recipient.win AS recipient_win,
+			recipient.lose AS recipient_lose,
+			recipient.elo AS recipient_elo
+		FROM "Message" m
+		JOIN "User" sender ON sender.id = m."senderId"
+		LEFT JOIN "User" recipient ON recipient.id = m."recipientId"
+		JOIN "User" currentUser ON currentUser.username = ${username}
+		WHERE (m."senderId" = currentUser.id OR m."recipientId" = currentUser.id)
+		AND m."isGeneral" = FALSE
 	)
 	SELECT * FROM general_messages
 	UNION ALL
 	SELECT * FROM private_messages
-	ORDER BY "id" ASC`;
-
+	ORDER BY id ASC`;
 
 	return (messages);
 }
+
