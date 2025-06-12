7140fd97611e9e1b5255f3b24b95f27a5bbcc42b
hugo <hugodelacour.pro@gmail.com>
2025-06-12 17:19:39 +0200

    fix: locale es

--- locales/server.ts
+++ locales/server.ts
@@ -1,10 +1,11 @@
 // locales/server.ts
 import { createI18nServer } from 'next-international/server'
 
-export const locales = ['en', 'fr'] as const
+export const locales = ['en', 'fr', 'es'] as const
 export type Locale = (typeof locales)[number]
 
 export const { getI18n, getScopedI18n, getStaticParams } = createI18nServer({
   en: () => import('./en'),
   fr: () => import('./fr'),
+  es: () => import('./es'),
 })
