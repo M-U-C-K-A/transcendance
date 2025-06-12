73403d5c60a4bdce14eebfc4a836555e5e41927d
Thomas Robert <throbert@c2r8p6.42lehavre.fr>
2025-06-12 03:06:38 +0200

    Add: Autamated SSL

---
+++ generate_ssl.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+CERT_DIR=/etc/nginx/certs
+CERT_FILE=$CERT_DIR/cert.pem
+KEY_FILE=$CERT_DIR/key.pem
+
+if [ ! -f "$CERT_FILE" ] || [ ! -f "$KEY_FILE" ]; then
+  echo "Generating self-signed certificate..."
+  mkdir -p $CERT_DIR
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout $KEY_FILE \
+    -out $CERT_FILE \
+    -subj "/CN=localhost"
+else
+  echo "Certificate already exists."
+fi
+
+# Lancement de nginx en mode foreground
+nginx -g "daemon off;"
