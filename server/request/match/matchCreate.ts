4ef002e601728c357a9ff7eb9bd256ad1bafb4eb
rbardet <robinbt2004@gmail.com>
2025-06-12 20:12:55 +0200

    Add: Create Match Route

---
+++ server/request/match/matchCreate.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client'
+import { userAgent } from 'next/server'
+
+const Prisma = new PrismaClient()
+
+export default async function matchCreate(matchName: string, hostId: number) {
+	const hostData = await Prisma.user.findUnique ({
+		where: {
+			id: hostId,
+		},
+		select: {
+			elo: true,
+		}
+	});
+
+	if (!hostData) {
+		throw new Error ("t'es qui ptdrrrrrr ?")
+	}
+
+	const matchInfo = await Prisma.match.create ({
+		data: {
+			name: matchName,
+			p1Id: hostId,
+			p1Elo: hostData.elo
+		},
+		select: {
+			id: true,
+		},
+	});
+
+	return (matchInfo)
+}
