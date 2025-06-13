1731f19aec51968e1c045cf63ad94bfdb0a5286b
rbardet <robinbt2004@gmail.com>
2025-06-13 20:13:10 +0200

    Fix: game creation

--- server/routes/auth/2FAregisterRoute.ts
+++ server/routes/auth/2FAregisterRoute.ts
@@ -2,12 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { connectionData } from './interface'
 import register2FA from '@/server/request/auth/2FAregister'
 
-export default async function registerRoute(server: FastifyInstance) {
+export default async function register2FARoute(server: FastifyInstance) {
 	server.post('/auth/register2FA', async (request, reply) => {
 		const data = request.body as connectionData
 
 		try {
-			await register2FA(data)
+			const result = await register2FA(data)
 			return reply.code(201).send({ message: 'User registered successfully' })
 		} catch (err: any) {
 			if (err.message === 'Username already taken') {
