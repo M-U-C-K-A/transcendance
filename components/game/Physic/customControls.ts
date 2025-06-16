fc73528a5a896b845fcac127fad0b0897673d812
ThomasRbm <frozyxyt.76@gmail.com>
2025-06-16 03:20:50 +0200

    Update customControls.ts

--- components/game/Physic/customControls.ts
+++ components/game/Physic/customControls.ts
@@ -18,6 +18,9 @@ let currentControls: Controls = {
   player2Special: 'ArrowLeft',
 };
 
+
+// fonction utilise dans les menu de maj de touche 
+// currentControls = le contexte des touche du jeu
 export function updateControls(newControls: Controls) {
   // Mise à jour des contrôles
   currentControls = newControls;
@@ -26,21 +29,28 @@ export function updateControls(newControls: Controls) {
   keys.clear();
 }
 
+
+// has permet de verif. un Set (voir assignation de key dans input)
+// return la key
 export function isPlayer1UpPressed(): boolean {
   return keys.has(currentControls.player1Up.toLowerCase());
 }
+
 
 export function isPlayer1DownPressed(): boolean {
   return keys.has(currentControls.player1Down.toLowerCase());
 }
 
+
 export function isPlayer2UpPressed(): boolean {
   return keys.has(currentControls.player2Up.toLowerCase());
 }
 
+
 export function isPlayer2DownPressed(): boolean {
   return keys.has(currentControls.player2Down.toLowerCase());
 }
+
 
 export function getCurrentControls(): Controls {
   return { ...currentControls };
