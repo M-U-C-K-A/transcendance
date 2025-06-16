c506287f578994ee9558ea7224af86082d56a971
Thomasrbm <frozyxyt.76@gmail.com>
2025-06-16 03:19:32 +0200

    Update countdown.ts

--- components/game/Physic/countdown.ts
+++ components/game/Physic/countdown.ts
@@ -1,28 +1,54 @@
+
+//  call back possible (serve par ex)
+
 export function startCountdown(
   duration: number,
   setIsPaused: (isPaused: boolean) => void,
   setCountdown: (countdown: number | null) => void,
   callback: () => void
-) {
+) 
+{
+
+
   setIsPaused(true);
   setCountdown(duration);
-  
+
+
+
   // Créer un état pour bloquer les changements de pause
   const blockPauseChanges = { current: true };
-  
+
   let cnt = duration;
-  const iv = setInterval(() => {
-    cnt--;
-    if (cnt > 0) {
+
+  // setInterval == fonction typescript   va activer le code suivant toutes les ms donne en fin
+  const iv = setInterval(() => 
+  {
+
+    cnt--; 
+
+
+    // actualise le coutdown  ref dans pong3d et envoye dans GameUI
+    if (cnt > 0) 
+    {
       setCountdown(cnt);
-    } else {
-      clearInterval(iv);
-      setCountdown(null);
-      setIsPaused(false);
+    } 
+
+
+
+    else 
+    {
+      clearInterval(iv); // ft ts qui le stop en fin de chiffre
+      setCountdown(null); // met a null. pour l affichage game UI
+      setIsPaused(false); // remet en marche 
       blockPauseChanges.current = false; // Débloque après le décompte
-      callback();
+      callback(); // active la fonction si y a
     }
-  }, 500);
+
+
+
+  }, 500); // inteval de demi seconde entre les chiffres 
+
+
 
   // Retourne la fonction pour débloquer si nécessaire
   return () => {
