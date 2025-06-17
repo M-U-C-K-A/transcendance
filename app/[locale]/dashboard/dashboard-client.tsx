454097dba1fb3992cf008a6f9ec83b1dfb2ada86
rbardet <robinbt2004@gmail.com>
2025-06-17 02:30:01 +0200

    Tournament finished

--- app/[locale]/dashboard/dashboard-client.tsx
+++ app/[locale]/dashboard/dashboard-client.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useEffect } from "react"
 import { Header } from "@/components/dashboard/Header"
 import { UserProfile } from "@/components/dashboard/UserProfile"
 import { ColleaguesList } from "@/components/dashboard/Colleagues/ColleaguesList"
@@ -7,8 +8,27 @@ import { GameTabs } from "@/components/dashboard/GameTabs"
 import { ChatSection } from "@/components/dashboard/ChatSection"
 
 export default function DashboardClient({ locale, jwtToken }: { locale: string, jwtToken: string }) {
-  localStorage.removeItem("currentGameId");
-  localStorage.removeItem("currentGameName");
+  useEffect(() => {
+    if (localStorage.getItem("currentGameId")) {
+      localStorage.removeItem("currentGameId");
+    }
+    if (localStorage.getItem("currentGameName")) {
+      localStorage.removeItem("currentGameName");
+    }
+    if (localStorage.getItem("tournamentBracket")) {
+      localStorage.removeItem("tournamentBracket");
+    }
+    if (localStorage.getItem("tournamentId")) {
+      localStorage.removeItem("tournamentId");
+    }
+    if (localStorage.getItem("tournamentParticipants")) {
+      localStorage.removeItem("tournamentParticipants");
+    }
+    if (localStorage.getItem("tournamentSlot")) {
+      localStorage.removeItem("tournamentSlot");
+    }
+  }, []);
+
   return (
     <div className="bg-background min-h-screen">
       <Header locale={locale} />
