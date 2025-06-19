d3e442e70e26b04a58948b2e79a6b93feee1b234
rbardet <robinbt2004@gmail.com>
2025-06-19 18:02:04 +0200

    Add: gdpr get data

---
+++ Back/server/routes/gdpr/getDataRoute.ts
@@ -0,0 +1,22 @@
+import authMiddleware from "@/server/authMiddleware";
+import getData from "@/server/request/gdpr/getData";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+
+export default async function getDataRoute(server: FastifyInstance) {
+	server.post('/gdpr/getdata', { preHandler: authMiddleware }, async function (request: FastifyRequest, reply: FastifyReply) {
+	const user = request.user as { id: number }
+
+	if (!user) {
+		return reply.code(400).send({ error: 'parameter is required' })
+	}
+
+	try {
+		const result = await getData(user.id)
+		return (reply.code(200).send({result}))
+	} catch (err: any) {
+		if (err.message === 'User not found') {
+			return reply.code(404).send({ error: 'User not found' })
+		} return reply.code(500).send({ error: 'Internal server error' })
+	}
+})
+}
