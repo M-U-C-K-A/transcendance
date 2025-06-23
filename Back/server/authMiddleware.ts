f38bcd4a62cdf1388017ea4e6f40e7e40ecf1527
rbardet <robinbt2004@gmail.com>
2025-06-23 19:45:35 +0200

    Fix: middleware

--- Back/server/authMiddleware.ts
+++ Back/server/authMiddleware.ts
@@ -3,22 +3,16 @@ import { JWTPayload } from '@/types/jwt'
 
 export default async function authMiddleware(request: FastifyRequest, reply: FastifyReply) {
 	try {
-		console.log("ALL COOKIES:", request.cookies)
 		const token = request.cookies.token
-		console.log("Token extracted from cookies:", token)
 
 		if (!token) {
-			console.log("No token found in cookies!")
 			return reply.status(401).send({ error: 'Authentication token missing' })
 		}
 
 		const decoded = request.server.jwt.verify<JWTPayload>(token)
 
-		console.log("JWT decoded payload:", decoded)
-
 		request.user = decoded
 	} catch (err) {
-		console.log("Error during jwt verify:", err)
 		return reply.status(401).send({ error: 'Invalid or expired token' })
 	}
 }
